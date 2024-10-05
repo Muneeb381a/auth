1ae0adeaa49dbd5bc9ce8688b7cef67a81afa2dd
Muneeb381a <muneebafzal381a@gmail.com>
2024-10-05 23:45:37 +0500

    creating routes

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,15 @@ import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv"
 
 dotenv.config();
+
+import authRoutes from "./routes/auth.route.js"
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hi there")
 })
 
-app.arguments("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 
 app.listen(8000, () => {
     connectDB()
