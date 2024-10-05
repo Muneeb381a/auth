24e7bc27cbd3b5198f5a0d3403303e277170033f
Muneeb381a <muneebafzal381a@gmail.com>
2024-10-05 23:25:27 +0500

    create a mongo db connection

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 import express from "express"
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv"
 
+dotenv.config();
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi there")
+})
+
+app.arguments("/api/auth", authRoutes)
+
+app.listen(8000, () => {
+    connectDB()
+    console.log("server is running on port");
+})
